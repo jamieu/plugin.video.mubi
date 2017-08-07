d5dc1eec30012ee6e4717810a51eb611ad9f8171
Alex Bush <alex.bush@coxauto.co.uk>
2017-08-07 09:38:49 +0100

    fix for getting the reel id

--- resources/lib/mubi.py
+++ resources/lib/mubi.py
@@ -7,6 +7,7 @@ import hashlib
 from collections import namedtuple
 from kodiswift import xbmc
 from urlparse import urljoin
+
 try:
     from simplecache import SimpleCache
 except:
@@ -35,7 +36,6 @@ class Mubi(object):
         self._simplecache = SimpleCache()
         # Need a 20 digit id, hash username to make it predictable
         self._udid = int(hashlib.sha1(username).hexdigest(), 32) % (10 ** 20)
-        self._film_info = {}
         self._token = None
         self.login()
 
@@ -65,7 +65,6 @@ class Mubi(object):
 
     def get_film_metadata(self, film_overview):
         film_id = film_overview['id']
-
         available_at = dateutil.parser.parse(film_overview['available_at'])
         expires_at = dateutil.parser.parse(film_overview['expires_at'])
         # Check film is valid, has not expired and is not preview
@@ -78,18 +77,16 @@ class Mubi(object):
             return None
         hd = film_overview['hd']
         drm = film_overview['default_reel']['drm']
-        self._film_info[film_id] = {"reel_id": film_overview['default_reel']['id'], "is_drm": drm}
         audio_lang = film_overview['default_reel']['audio_language']
         subtitle_lang = film_overview['default_reel']['subtitle_language']
         # Build plot field. Place lang info in here since there is nowhere else for it to go
         drm_string = "Warning: this film cannot be played since it uses DRM\n" if drm else ""
-        lang_string = ("Language: %s" % audio_lang) + (", Subtitles: %s\n" % subtitle_lang) if subtitle_lang else "\n"
+        lang_string = ("Language: %s" % audio_lang) + ((", Subtitles: %s\n" % subtitle_lang) if subtitle_lang else "\n")
         plot_string = "Synopsis: %s\n\nOur take: %s" % (film_overview['excerpt'], film_overview['editorial'])
-
         # Get detailed look at film to get cast info
         film_page = self.get_film_page(film_id)
         cast = [(m['name'], m['credits']) for m in film_page['cast']]
-
+        # Build film metadata object
         metadata = Metadata(
             title=film_overview['title'],
             director=film_overview['directors'],
@@ -108,22 +105,37 @@ class Mubi(object):
         listview_title = film_overview['title'] + (" [HD]" if hd else "")
         return Film(listview_title, film_id, film_overview['stills']['standard'], metadata)
 
-    def now_showing(self):
+    def get_now_showing_json(self):
         # Get list of available films
         args = "?client=android&country=GB&token=%s&udid=%s&client_version=3.05" % (self._token, self._udid)
         r = requests.get(self._mubi_urls['films'] + args)
         if r.status_code != 200:
             xbmc.log("Invalid status code %s getting list of films")
-        films = [self.get_film_metadata(film) for film in json.loads(r.text)]
+        return r.text
+
+    def now_showing(self):
+        films = [self.get_film_metadata(film) for film in json.loads(self.get_now_showing_json())]
         return [f for f in films if f]
 
+    def get_default_reel_id_is_drm(self, film_id):
+        reel_id = [(f['default_reel']['id'], f['default_reel']['drm'])
+                   for f in json.loads(self.get_now_showing_json()) if f['id'] == film_id]
+        if len(reel_id) == 1:
+            return reel_id[0]
+        elif reel_id:
+            xbmc.log("Multiple default_reel's returned for film %s: %s" % (film_id, ', '.join(reel_id)))
+        else:
+            xbmc.log("Could not find default reel id for film %s" % film_id)
+            return None
+
     def get_play_url(self, film_id):
+        (reel_id, is_drm) = self.get_default_reel_id_is_drm(film_id)
         args = "?client=android&country=GB&token=%s&udid=%s&client_version=3.05&film_id=%s&reel_id=%s&download=false" \
-               % (self._token, self._udid, film_id, self._film_info[film_id]["reel_id"])
+               % (self._token, self._udid, film_id, reel_id)
         r = requests.get((self._mubi_urls['viewing'] % str(film_id)) + args)
         if r.status_code != 200:
             xbmc.log("Could not get secure URL for film %s" % film_id)
-        url = json.loads(r.text).url
+        url = json.loads(r.text)["url"]
         # For DRM you will have to find the following info:
         # {"userId": long(result['username']), "sessionId": result['transaction'], "merchant": result['accountCode']}
         # This might need optdata in header however looking in requests during browser negotiation I don't see it
@@ -131,6 +143,6 @@ class Mubi(object):
         # The best conversation for this is:
         # https://github.com/emilsvennesson/kodi-viaplay/issues/9
         # You can pick this conversation up using Android Packet Capture
-        item_result = {'url': url, 'is_mpd': "mpd" in url, 'is_drm': self._film_info[film_id]["is_drm"]}
+        item_result = {'url': url, 'is_mpd': "mpd" in url, 'is_drm': is_drm}
         xbmc.log("Got video info as: '%s'" % json.dumps(item_result))
         return item_result
